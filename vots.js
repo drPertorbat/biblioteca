a55eaeb5a75fee1cc4a40c95a78212183d6128f3
oriol <oriol.rocafort@3asie.es>
2023-03-15 10:02:41 +0100

    Opció per afegir votacions - WIP

--- vots.js
+++ vots.js
@@ -3,20 +3,51 @@ const user = 'entry.2132131866';
 const obra = 'entry.1693883818';
 const rank = 'entry.437166774';
 
+var userCode;
+
 $().ready(function(){
+	
+	userCode = localStorage.getItem("ruser");
+	
+	if(!userCode){
+		userCode = makeid(5);
+		localStorage.setItem('ruser', userCode);		
+	}
+	
+	
+	$('.rank span').click(function(){
+		var val = $(this).text();
+		var text = $(this).parent().data("target");
+		
+		testVot(text, val);
+	});
 	
 	
 	
 });
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
 
-function vot(text, val) {
+
+
+function testVot(text, val) {
+	
 	var newForm = jQuery('<form>', {
         'action': url,
         'target': '_top'
     }).append(jQuery('<input>', {
         'name': user,
-        'value': 'test-user',
+        'value': userCode,
         'type': 'hidden'
     })).append(jQuery('<input>', {
         'name': obra,
@@ -26,36 +57,34 @@ function vot(text, val) {
         'name': rank,
         'value': val,
         'type': 'hidden'
-    }));
+    }));	
 	
-	$("body").append(newForm);
-    newForm.submit();
-	
-    $("body form").remove();
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        crossdomain: false,
+        dataType: 'json',
+        data: newForm.serialize(),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        statusCode: { //the status code from the POST request
+            0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+              //success
+              console.log('hooray!');
+            }, 
+            200: function(data) {//200 is a success code. it went through!
+              //success
+            	console.log('hooray!');
+            },
+            403: function(data) {//403 is when something went wrong and the submission didn't go through
+              //error
+            	console.log('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
+            }
+          }  
+    });
+
+    return false;
 }
 
-function postData(text, val) {
-	var form = new FormData();
-	form.append("entry.2132131866", "usuari1");
-	form.append("entry.1693883818", "La pell freda");
-	form.append("entry.437166774", "4");
-
-	var settings = {
-	  "async": true,
-	  "crossDomain": true,
-	  "url": "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf0r03S6TFdWsQsHf6HPYdanbTkaRQ2Mt_h4fsHjcqrNzhqlw/formResponse",
-	  "method": "POST",
-	  "headers": {
-	    "cache-control": "no-cache",
-	    "Postman-Token": "e65af156-557a-400c-871c-9ca0108eb790",
-	  },
-	  "processData": false,
-	  "contentType": false,
-	  "mimeType": "multipart/form-data",
-	  "data": form
-	}
-
-	$.ajax(settings).done(function (response) {
-	  console.log(response);
-	});
-}
